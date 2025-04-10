fbc0f19444a626a247be7d4757328d7c0d745b12
AleksandraChern <Sashulya81@list.ru>
2025-04-10 17:39:58 +0200

    vi

--- dz/dz23/dz23.py
+++ dz/dz23/dz23.py
@@ -5,10 +5,10 @@ import os
 file_path = "test/text.txt"
 
 if os.path.exists(file_path):
-    ...
-    # directory, name = os.path.split(file_path)
-    # atime = os.path.getatime(file_path)
-    # print(f"{name} ({directory}) - время последнего доступа к файлу {atime} секунд")
+    directory, name = os.path.split(file_path)
+    atime = os.path.getatime(file_path)
+    # print(name)
+    print(f"{name} ({directory}) - last access file {atime} sec")
 else:
     print(f"Файл {file_path} отсутствует")
 
